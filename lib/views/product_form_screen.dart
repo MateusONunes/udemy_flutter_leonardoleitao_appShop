363fdfbf42b7f0436a74c6c66e207c18c81426be
Mateus Nunes <mateus.2006@gmail.com>
2020-06-09 17:08:00 -0300

    Aplicado "Seção 11: Adicionando Autenticação" do Curso "Flutter e dart - Aprenda e Crie APPs iOS e Android 2020"
    https://www.udemy.com/course/curso-flutter/learn/lecture/20231476#questions

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -57,9 +57,8 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
     bool endsWithPng = url.toLowerCase().endsWith('.png');
     bool endsWithJpg = url.toLowerCase().endsWith('.jpg');
     bool endsWithJpeg = url.toLowerCase().endsWith('.jpeg');
-    bool endsWithWebp = url.toLowerCase().endsWith('.webp');
     return (startWithHttp || startWithHttps) &&
-        (endsWithPng || endsWithJpg || endsWithJpeg || endsWithWebp);
+        (endsWithPng || endsWithJpg || endsWithJpeg);
   }
 
   @override
